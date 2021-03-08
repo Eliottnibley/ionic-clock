df90da6a360d24c18e4338b862ed2f65bf35d69f
Eliottnibley <e.nibley@gmail.com>
2021-03-08 12:44:18 -0700

    update

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -10,8 +10,8 @@ const Landing: React.FC = () => {
       <IonContent style={{'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center'}}>
         <IonText color='' style={{'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center', 'textAlign':'center'}}>The app features custom alarms, convinient stopwatch, and easy an easy to use timer. Use the tabs at the bottom to explore!</IonText>
       </IonContent>
-      <IonContent style={{'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center'}}>
-        <IonTitle>For the best expirience, view page in mobile device view.</IonTitle>
+      <IonContent style={{'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center', 'textAlign':'center'}}>
+        <IonText>For the best expirience, view page in mobile device view.</IonText>
       </IonContent>
     </IonPage>
   )
